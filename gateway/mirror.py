NousResearch/hermes-agent

fix(gateway): mirror_to_session reports False when the transcript write fails
`_append_to_sqlite` caught and debug-logged its own exceptions, so the outer
handler in `mirror_to_session` never fired and every failed SQLite write was
reported as a successful mirror. Callers (cron in_channel seed, send_message)
had no way to know the transcript was never updated.

Let the write helper raise; the caller already warns and returns False.

Fixes #10130

gateway/mirror.py
@@ -115,14 +115,15 @@ def _find_session_id(platform: str, chat_id: str, thread_id: Optional[str] = Non
 
 
 def _append_to_sqlite(session_id: str, message: dict) -> None:
-    """Append a message to the SQLite session database."""
-    try:
-        from hermes_state_registry import acquire, release_or_close
+    """Append a message to the SQLite session database.
 
-        db = acquire()
-        try:
-            db.append_message(session_id=session_id, role=message.get("role", "assistant"), content=message.get("content"))
-        finally:
-            release_or_close(db)
-    except Exception as e:
-        logger.debug("Mirror SQLite write failed: %s", e)
+    Raises on failure: ``mirror_to_session`` reports ``False`` (and warns) only when the
+    exception reaches it — swallowing it here made every failed write look mirrored (#10130).
+    """
+    from hermes_state_registry import acquire, release_or_close
+
+    db = acquire()
+    try:
+        db.append_message(session_id=session_id, role=message.get("role", "assistant"), content=message.get("content"))
+    finally:
+        release_or_close(db)

tests/gateway/test_mirror.py
@@ -118,6 +118,29 @@ class TestMirrorToSession:
         assert result is False
 
 
+    def test_failed_sqlite_write_reports_false(self, tmp_path):
+        """A mirror whose transcript write raises must not report success (#10130)."""
+        sessions_dir, index_file = _setup_sessions(tmp_path, {
+            "dm": {
+                "session_id": "sess_dm",
+                "origin": {"platform": "telegram", "chat_id": "123"},
+                "updated_at": "2026-01-01T00:00:00",
+            },
+        })
+        broken_db = MagicMock()
+        broken_db.find_session_by_origin.return_value = None  # resolve via sessions.json
+        broken_db.append_message.side_effect = OSError("disk full")
+
+        with patch.object(mirror_mod, "_SESSIONS_DIR", sessions_dir), \
+             patch.object(mirror_mod, "_SESSIONS_INDEX", index_file), \
+             patch("hermes_state_registry.acquire", return_value=broken_db), \
+             patch("hermes_state_registry.release_or_close"):
+            result = mirror_to_session("telegram", "123", "Hello!")
+
+        assert result is False
+        broken_db.append_message.assert_called_once()
+
+
 class TestAppendToSqlite:
     def test_connection_is_released_after_use(self, tmp_path):
         """Verify _append_to_sqlite returns the shared SessionDB reference."""

loteiron/ZeusAgent

Upstream-Commit: 6716f2239637dc4c524b4bab40192035c26b8b0f
Upstream-Repository: https://github.com/NousResearch/hermes-agent

gateway/mirror.py
@@ -115,14 +115,15 @@ def _find_session_id(platform: str, chat_id: str, thread_id: Optional[str] = Non
 
 
 def _append_to_sqlite(session_id: str, message: dict) -> None:
-    """Append a message to the SQLite session database."""
-    try:
-        from zeus_state_registry import acquire, release_or_close
+    """Append a message to the SQLite session database.
 
-        db = acquire()
-        try:
-            db.append_message(session_id=session_id, role=message.get("role", "assistant"), content=message.get("content"))
-        finally:
-            release_or_close(db)
-    except Exception as e:
-        logger.debug("Mirror SQLite write failed: %s", e)
+    Raises on failure: ``mirror_to_session`` reports ``False`` (and warns) only when the
+    exception reaches it — swallowing it here made every failed write look mirrored (#10130).
+    """
+    from zeus_state_registry import acquire, release_or_close
+
+    db = acquire()
+    try:
+        db.append_message(session_id=session_id, role=message.get("role", "assistant"), content=message.get("content"))
+    finally:
+        release_or_close(db)

tests/gateway/test_mirror.py
@@ -118,6 +118,29 @@ class TestMirrorToSession:
         assert result is False
 
 
+    def test_failed_sqlite_write_reports_false(self, tmp_path):
+        """A mirror whose transcript write raises must not report success (#10130)."""
+        sessions_dir, index_file = _setup_sessions(tmp_path, {
+            "dm": {
+                "session_id": "sess_dm",
+                "origin": {"platform": "telegram", "chat_id": "123"},
+                "updated_at": "2026-01-01T00:00:00",
+            },
+        })
+        broken_db = MagicMock()
+        broken_db.find_session_by_origin.return_value = None  # resolve via sessions.json
+        broken_db.append_message.side_effect = OSError("disk full")
+
+        with patch.object(mirror_mod, "_SESSIONS_DIR", sessions_dir), \
+             patch.object(mirror_mod, "_SESSIONS_INDEX", index_file), \
+             patch("zeus_state_registry.acquire", return_value=broken_db), \
+             patch("zeus_state_registry.release_or_close"):
+            result = mirror_to_session("telegram", "123", "Hello!")
+
+        assert result is False
+        broken_db.append_message.assert_called_once()
+
+
 class TestAppendToSqlite:
     def test_connection_is_released_after_use(self, tmp_path):
         """Verify _append_to_sqlite returns the shared SessionDB reference."""