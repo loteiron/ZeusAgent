zhouhe-xydt/hermes-agent

fix(config): treat explicit false values in HERMES_MANAGED as unmanaged
Previously, setting HERMES_MANAGED=false (or 0, no, off) would be
interpreted as a literal managed-system name, causing is_managed()
to incorrectly return True and block update/config commands.

- Add _MANAGED_FALSE_VALUES tuple for canonical false strings
- Check false values before true values in get_managed_system()
- Add parametrized regression tests for all false variants

Fixes #12864

tests/hermes_cli/test_managed_installs.py
@@ -1,9 +1,12 @@
 from types import SimpleNamespace
 from unittest.mock import patch
 
+import pytest
+
 from hermes_cli.config import (
     format_managed_message,
     get_managed_system,
+    is_managed,
     recommended_update_command,
 )
 from hermes_cli.main import cmd_update
@@ -42,6 +45,15 @@ def test_cmd_update_blocks_managed_homebrew(monkeypatch, capsys):
     captured = capsys.readouterr()
     assert "managed by Homebrew" in captured.err
     assert "brew upgrade hermes-agent" in captured.err
+
+
+@pytest.mark.parametrize("false_value", ["false", "0", "no", "off", "FALSE", "False"])
+def test_get_managed_system_false_values(monkeypatch, false_value):
+    monkeypatch.setenv("HERMES_MANAGED", false_value)
+
+    assert get_managed_system() is None
+    assert not is_managed()
+    assert recommended_update_command() == "hermes update"
 
 
 def test_optional_skill_source_honors_env_override(monkeypatch, tmp_path):

loteiron/ZeusAgent

Upstream-Commit: 92a83980878ef568d314e4ac50793bf0c8b2c459
Upstream-Repository: https://github.com/NousResearch/hermes-agent

tests/zeus_cli/test_managed_installs.py
@@ -1,7 +1,9 @@
 from types import SimpleNamespace
 from unittest.mock import patch
 
-from zeus_cli.config import recommended_update_command
+import pytest
+
+from zeus_cli.config import get_managed_system, is_managed, recommended_update_command
 from zeus_cli.main import cmd_update
 from tools.skills_hub_official import OptionalSkillSource
 
@@ -16,6 +18,17 @@ def test_recommended_update_command_defaults_to_zeus_update(monkeypatch):
     # detect_install_method().
     with patch("zeus_cli.config.get_managed_update_command", return_value=None), \
          patch("zeus_cli.config.detect_install_method", return_value="git"):
+        assert recommended_update_command() == "zeus update"
+
+
+@pytest.mark.parametrize("false_value", ["false", "0", "no", "off", "FALSE"])
+def test_get_managed_system_false_values(monkeypatch, false_value):
+    """An explicit opt-out is not a package manager named "false" (#12864)."""
+    monkeypatch.setenv("ZEUS_MANAGED", false_value)
+
+    assert get_managed_system() is None
+    assert not is_managed()
+    with patch("zeus_cli.config.detect_install_method", return_value="git"):
         assert recommended_update_command() == "zeus update"
 
 