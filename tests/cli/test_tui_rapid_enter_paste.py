NousResearch/hermes-agent

fix(cli): Enter arriving mid-paste is a newline, not a per-line steer
Without bracketed paste (tmux strips it; IME/voice dictation emits keys
one at a time) each pasted newline reaches _tui_handle_enter as its own
Enter event, so a multi-line paste while the agent ran became N separate
steering messages. An Enter within 50 ms of the last buffer change is
text still arriving; insert the newline and let the buffer accumulate.
Humans type >50 ms apart, so a real submit is unaffected.

Same mechanism as #10997 (@iRonin) and #19498 (@Ralojeil1), which
targeted the removed classic-CLI handle_enter.

Refs #10994

tests/cli/test_tui_rapid_enter_paste.py
@@ -1,0 +1,51 @@
+"""Enter arriving mid-paste (no bracketed paste; each newline is its own key event) must not
+submit / steer the partial line (#10994). Only the Enter-vs-recent-text-change timing is exercised;
+the real prompt_toolkit Buffer carries the text so the newline lands where the user's did."""
+from __future__ import annotations
+
+import time
+from types import SimpleNamespace
+from unittest.mock import MagicMock
+
+from prompt_toolkit.buffer import Buffer
+
+from hermes_cli.cli_tui_mixin import _RAPID_INPUT_ENTER_WINDOW_S
+
+
+def _shell():
+    from cli import HermesCLI
+    shell = object.__new__(HermesCLI)
+    shell._tui_enter_overlay = lambda event: False
+    shell._tui_multiline_shortcuts = False
+    shell._attached_images = []
+    shell._agent_running = True
+    shell._tui_enter_while_busy = MagicMock()
+    shell._tui_enter_inline_command = lambda *a, **k: False
+    shell._inline_pastes = lambda buf: None
+    shell._tui_paste_counter = 0
+    shell.config = {}
+    shell._tui_prev_text_len = shell._tui_prev_newline_count = 0
+    shell._tui_paste_just_collapsed = shell._skip_paste_collapse = False
+    return shell
+
+
+def _event(buf: Buffer):
+    return SimpleNamespace(app=SimpleNamespace(current_buffer=buf, invalidate=lambda: None, is_running=False))
+
+
+def test_enter_right_after_text_arrives_is_a_newline_not_a_steer():
+    """Line 1 of a paste followed by Enter within the window stays in the buffer; the same Enter
+    after a human-scale pause submits the accumulated multi-line message once."""
+    shell = _shell()
+    buf = Buffer()
+    buf.on_text_changed += shell._tui_on_text_changed
+    buf.insert_text("line one")
+    shell._tui_handle_enter(_event(buf))          # < 50 ms after the text change
+    assert buf.text == "line one\n"
+    shell._tui_enter_while_busy.assert_not_called()
+
+    buf.insert_text("line two")
+    time.sleep(_RAPID_INPUT_ENTER_WINDOW_S * 3)  # the paste has finished; a human presses Enter
+    shell._tui_handle_enter(_event(buf))
+    shell._tui_enter_while_busy.assert_called_once()
+    assert shell._tui_enter_while_busy.call_args.args[0] == "line one\nline two"

loteiron/ZeusAgent

Upstream-Commit: 40f123ceab41905504e9800367991d61691377f0
Upstream-Repository: https://github.com/NousResearch/hermes-agent

tests/cli/test_tui_rapid_enter_paste.py
@@ -1,0 +1,51 @@
+"""Enter arriving mid-paste (no bracketed paste; each newline is its own key event) must not
+submit / steer the partial line (#10994). Only the Enter-vs-recent-text-change timing is exercised;
+the real prompt_toolkit Buffer carries the text so the newline lands where the user's did."""
+from __future__ import annotations
+
+import time
+from types import SimpleNamespace
+from unittest.mock import MagicMock
+
+from prompt_toolkit.buffer import Buffer
+
+from zeus_cli.cli_tui_mixin import _RAPID_INPUT_ENTER_WINDOW_S
+
+
+def _shell():
+    from cli import ZeusAgentCLI
+    shell = object.__new__(ZeusAgentCLI)
+    shell._tui_enter_overlay = lambda event: False
+    shell._tui_multiline_shortcuts = False
+    shell._attached_images = []
+    shell._agent_running = True
+    shell._tui_enter_while_busy = MagicMock()
+    shell._tui_enter_inline_command = lambda *a, **k: False
+    shell._inline_pastes = lambda buf: None
+    shell._tui_paste_counter = 0
+    shell.config = {}
+    shell._tui_prev_text_len = shell._tui_prev_newline_count = 0
+    shell._tui_paste_just_collapsed = shell._skip_paste_collapse = False
+    return shell
+
+
+def _event(buf: Buffer):
+    return SimpleNamespace(app=SimpleNamespace(current_buffer=buf, invalidate=lambda: None, is_running=False))
+
+
+def test_enter_right_after_text_arrives_is_a_newline_not_a_steer():
+    """Line 1 of a paste followed by Enter within the window stays in the buffer; the same Enter
+    after a human-scale pause submits the accumulated multi-line message once."""
+    shell = _shell()
+    buf = Buffer()
+    buf.on_text_changed += shell._tui_on_text_changed
+    buf.insert_text("line one")
+    shell._tui_handle_enter(_event(buf))          # < 50 ms after the text change
+    assert buf.text == "line one\n"
+    shell._tui_enter_while_busy.assert_not_called()
+
+    buf.insert_text("line two")
+    time.sleep(_RAPID_INPUT_ENTER_WINDOW_S * 3)  # the paste has finished; a human presses Enter
+    shell._tui_handle_enter(_event(buf))
+    shell._tui_enter_while_busy.assert_called_once()
+    assert shell._tui_enter_while_busy.call_args.args[0] == "line one\nline two"