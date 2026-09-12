NousResearch/hermes-agent

fix(gateway): hot-serve reaches pooled Desktop backends; deleted profiles leave no stale runtime entries
- PUT /api/messaging/platforms on a pooled `hermes --profile X serve` arrives without
  ?profile= (Desktop local topology, #109088): resolve the hot-serve target from the
  process's own profile so the multiplexer is pinged and the UI skips the restart banner.
- A profile deleted while the reconcile lock was held by its own adapter connect was
  recorded back into served_profiles; re-check the live set before recording.
- Drop a deleted profile's `<name>:<platform>` runtime-status entries instead of leaving
  them as `stopped`.

tests/hermes_cli/test_web_server_messaging_profiles.py
@@ -305,3 +305,37 @@ def test_scoped_enablement_uses_only_own_credentials(client, isolated_profiles, 
         assert platform["enabled"] is enabled
         assert platform["configured"] is True
     assert "root-token" in (isolated_profiles["default"] / ".env").read_text(encoding="utf-8")
+
+
+@pytest.mark.parametrize("topology", ["scoped_query", "pooled_unscoped"])
+def test_credential_write_hot_serves_a_multiplexed_profile(client, isolated_profiles, monkeypatch, topology):
+    """A token saved for a profile the live multiplexer serves is handed to the multiplexer right
+    away (``hot_served``), so the UI skips its restart banner. Both Desktop topologies: the dashboard's
+    ``?profile=`` and a pooled ``hermes --profile X serve`` that receives the PUT unscoped (#109088)."""
+    import hermes_cli.gateway as gateway_cli
+    import hermes_cli.gateway_multiplex_served as served_mod
+    notified = []
+    monkeypatch.setattr(gateway_cli, "named_profile_served_by_running_multiplexer", lambda name=None: name == "worker_alpha")
+    monkeypatch.setattr(served_mod, "notify_multiplexer_profiles_changed", lambda name, **kw: notified.append(name) or ["default", name])
+    if topology == "pooled_unscoped":
+        monkeypatch.setattr(gateway_cli, "_current_profile_name", lambda: "worker_alpha")
+        params = {}
+    else:
+        params = {"profile": "worker_alpha"}
+    resp = client.put("/api/messaging/platforms/telegram", params=params,
+                      json={"enabled": True, "env": {"TELEGRAM_BOT_TOKEN": _VALID_WORKER_BOT_TOKEN}})
+    assert resp.status_code == 200
+    assert resp.json()["hot_served"] is True
+    assert notified == ["worker_alpha"]
+
+
+def test_credential_write_on_default_profile_is_not_hot_served(client, isolated_profiles, monkeypatch):
+    """The default profile is the multiplexer itself (its own adapters are restart-managed): never
+    claim a hot serve for it."""
+    import hermes_cli.gateway_multiplex_served as served_mod
+    monkeypatch.setattr(served_mod, "notify_multiplexer_profiles_changed",
+                        lambda name, **kw: pytest.fail("default profile must not ping the multiplexer"))
+    resp = client.put("/api/messaging/platforms/telegram",
+                      json={"enabled": True, "env": {"TELEGRAM_BOT_TOKEN": _VALID_WORKER_BOT_TOKEN}})
+    assert resp.status_code == 200
+    assert resp.json()["hot_served"] is False

hermes_cli/web_routers/messaging.py
@@ -887,16 +887,20 @@ async def update_messaging_platform(platform_id: str, body: MessagingPlatformUpd
         )
         # A live multiplexer serving this named profile builds the adapter from the new token now
         # (its periodic rescan would otherwise pick it up within a cycle); no gateway restart.
-        hot_served = bool(target_profile) and await asyncio.to_thread(_notify_multiplexer_hot_serve, target_profile)
+        hot_served = await asyncio.to_thread(_notify_multiplexer_hot_serve, target_profile)
         return {"ok": True, "platform": platform_id, "hot_served": hot_served}
 
 
-def _notify_multiplexer_hot_serve(profile: str) -> bool:
-    from hermes_cli.gateway import named_profile_served_by_running_multiplexer
+def _notify_multiplexer_hot_serve(profile: Optional[str]) -> bool:
+    """True when a live multiplexer serves the written profile and was told to rebuild its adapters.
+    Unscoped (no ``?profile=``) means THIS process's profile: Desktop routes a pooled
+    ``hermes --profile X serve`` without the query (#109088), so X must resolve here too."""
+    from hermes_cli.gateway import _current_profile_name, named_profile_served_by_running_multiplexer
     from hermes_cli.gateway_multiplex_served import notify_multiplexer_profiles_changed
-    if not named_profile_served_by_running_multiplexer(profile):
+    name = (profile or "").strip() or _current_profile_name()
+    if not name or name == "default" or not named_profile_served_by_running_multiplexer(name):
         return False
-    return notify_multiplexer_profiles_changed(profile) is not None
+    return notify_multiplexer_profiles_changed(name) is not None
 
 
 @router.post("/api/messaging/platforms/{platform_id}/test")

loteiron/ZeusAgent

Upstream-Commit: 2d121aa322fe49719c7bf88eab64675485138940
Upstream-Repository: https://github.com/NousResearch/hermes-agent

tests/zeus_cli/test_web_server_messaging_profiles.py
@@ -305,3 +305,37 @@ def test_scoped_enablement_uses_only_own_credentials(client, isolated_profiles, 
         assert platform["enabled"] is enabled
         assert platform["configured"] is True
     assert "root-token" in (isolated_profiles["default"] / ".env").read_text(encoding="utf-8")
+
+
+@pytest.mark.parametrize("topology", ["scoped_query", "pooled_unscoped"])
+def test_credential_write_hot_serves_a_multiplexed_profile(client, isolated_profiles, monkeypatch, topology):
+    """A token saved for a profile the live multiplexer serves is handed to the multiplexer right
+    away (``hot_served``), so the UI skips its restart banner. Both Desktop topologies: the dashboard's
+    ``?profile=`` and a pooled ``zeus --profile X serve`` that receives the PUT unscoped (#109088)."""
+    import zeus_cli.gateway as gateway_cli
+    import zeus_cli.gateway_multiplex_served as served_mod
+    notified = []
+    monkeypatch.setattr(gateway_cli, "named_profile_served_by_running_multiplexer", lambda name=None: name == "worker_alpha")
+    monkeypatch.setattr(served_mod, "notify_multiplexer_profiles_changed", lambda name, **kw: notified.append(name) or ["default", name])
+    if topology == "pooled_unscoped":
+        monkeypatch.setattr(gateway_cli, "_current_profile_name", lambda: "worker_alpha")
+        params = {}
+    else:
+        params = {"profile": "worker_alpha"}
+    resp = client.put("/api/messaging/platforms/telegram", params=params,
+                      json={"enabled": True, "env": {"TELEGRAM_BOT_TOKEN": _VALID_WORKER_BOT_TOKEN}})
+    assert resp.status_code == 200
+    assert resp.json()["hot_served"] is True
+    assert notified == ["worker_alpha"]
+
+
+def test_credential_write_on_default_profile_is_not_hot_served(client, isolated_profiles, monkeypatch):
+    """The default profile is the multiplexer itself (its own adapters are restart-managed): never
+    claim a hot serve for it."""
+    import zeus_cli.gateway_multiplex_served as served_mod
+    monkeypatch.setattr(served_mod, "notify_multiplexer_profiles_changed",
+                        lambda name, **kw: pytest.fail("default profile must not ping the multiplexer"))
+    resp = client.put("/api/messaging/platforms/telegram",
+                      json={"enabled": True, "env": {"TELEGRAM_BOT_TOKEN": _VALID_WORKER_BOT_TOKEN}})
+    assert resp.status_code == 200
+    assert resp.json()["hot_served"] is False

zeus_cli/web_routers/messaging.py
@@ -872,16 +872,20 @@ async def update_messaging_platform(platform_id: str, body: MessagingPlatformUpd
         )
         # A live multiplexer serving this named profile builds the adapter from the new token now
         # (its periodic rescan would otherwise pick it up within a cycle); no gateway restart.
-        hot_served = bool(target_profile) and await asyncio.to_thread(_notify_multiplexer_hot_serve, target_profile)
+        hot_served = await asyncio.to_thread(_notify_multiplexer_hot_serve, target_profile)
         return {"ok": True, "platform": platform_id, "hot_served": hot_served}
 
 
-def _notify_multiplexer_hot_serve(profile: str) -> bool:
-    from zeus_cli.gateway import named_profile_served_by_running_multiplexer
+def _notify_multiplexer_hot_serve(profile: Optional[str]) -> bool:
+    """True when a live multiplexer serves the written profile and was told to rebuild its adapters.
+    Unscoped (no ``?profile=``) means THIS process's profile: Desktop routes a pooled
+    ``zeus --profile X serve`` without the query (#109088), so X must resolve here too."""
+    from zeus_cli.gateway import _current_profile_name, named_profile_served_by_running_multiplexer
     from zeus_cli.gateway_multiplex_served import notify_multiplexer_profiles_changed
-    if not named_profile_served_by_running_multiplexer(profile):
+    name = (profile or "").strip() or _current_profile_name()
+    if not name or name == "default" or not named_profile_served_by_running_multiplexer(name):
         return False
-    return notify_multiplexer_profiles_changed(profile) is not None
+    return notify_multiplexer_profiles_changed(name) is not None
 
 
 @router.post("/api/messaging/platforms/{platform_id}/test")