NousResearch/hermes-agent

fix(desktop,dashboard): served profile's api_server/webhook read connected with their /p/<profile>/ URL; shared-gateway restart asks first
Under gateway.multiplex_profiles a secondary's api_server and webhook are never built as
adapters (run_adapters skips SHARED_LISTENER_MIRROR_PLATFORMS: the default's listener answers
/p/<profile>/...). The multiplexer record therefore has no `<profile>:api_server` entry,
profile_platforms_from_multiplexer() returned {} for them and both /api/messaging/platforms
and /api/status?profile= fell through to `pending_restart`: the Desktop Messaging card and
Command Center said "Restart needed" forever for a platform that was answering.

- gateway.status.shared_listener_mirror_platforms projects the default's LIVE api_server /
  webhook entry onto every served secondary with `ingress_url` = `<listener>/p/<profile>/v1`
  (`.../webhooks/<route>`); a dead default listener is not mirrored. The api_server / webhook
  adapters stamp the listener they actually bound (`listener_base`) on connect so the URL is
  the real one, not a config guess. `hermes status` lists those URLs beside the other
  shared-ingress platforms.
- /api/status?profile= reports `gateway_shared_with` (every profile the multiplexer carries)
  when the served rung answered; null for a standalone gateway.
- Desktop: the messaging card shows the URL line; "Restart gateway" from a served profile
  (statusbar menu, Cmd+K, messaging/webhooks banners, Command Center) confirms "Restart the
  shared gateway? All bots on this device reconnect: default, alpha, beta" (Restart all /
  Cancel) and toasts "Shared gateway restarted (3 bots)". Standalone keeps the silent path.
- Dashboard: same confirm + toast on the System page and the sidebar restart; the 409 from
  start/stop on a served profile renders as an inline notice instead of a raw error toast.

apps/desktop/src/lib/shared-gateway-restart.ts
@@ -1,0 +1,21 @@
+import type { StatusResponse } from '@/types/hermes'
+
+/** Profiles a gateway restart would blip when the polled profile is carried by the shared
+ *  multiplexer, in display order (default first, then as recorded). `null` for a standalone
+ *  gateway or an older backend that does not report `gateway_shared_with` — those keep the
+ *  plain no-dialog restart. A record naming only one profile is not shared either. */
+export function sharedGatewayProfiles(status: Pick<StatusResponse, 'gateway_shared_with'> | null | undefined): null | string[] {
+  const shared = status?.gateway_shared_with
+
+  if (!Array.isArray(shared)) {
+    return null
+  }
+
+  const names = [...new Set(shared.map(name => String(name).trim()).filter(Boolean))]
+
+  if (names.length < 2) {
+    return null
+  }
+
+  return names.sort((left, right) => (left === 'default' ? -1 : right === 'default' ? 1 : left.localeCompare(right)))
+}

apps/desktop/src/store/system-actions.ts
@@ -1,8 +1,10 @@
 import { atom } from 'nanostores'
 
-import { getActionStatus, restartGateway } from '@/hermes'
+import { getActionStatus, getStatus, restartGateway } from '@/hermes'
 import { translateNow } from '@/i18n'
-import { notifyError } from '@/store/notifications'
+import { sharedGatewayProfiles } from '@/lib/shared-gateway-restart'
+import { confirm } from '@/store/confirm'
+import { notify, notifyError } from '@/store/notifications'
 import type { ActionResponse } from '@/types/hermes'
 
 const POLL_ATTEMPTS = 18
@@ -34,6 +36,37 @@ async function awaitAction(name: string): Promise<void> {
   }
 }
 
+// Under `gateway.multiplex_profiles` the profile in view has no gateway of its
+// own: "Restart gateway" restarts the ONE shared multiplexer and every bot on
+// this device blips. Ask first, naming them; standalone gateways (and older
+// backends that do not report `gateway_shared_with`) keep the silent restart.
+// Resolves the served list when the user confirmed, `null` when nothing is
+// shared, `false` when they cancelled.
+export async function confirmSharedGatewayRestart(): Promise<false | null | string[]> {
+  let shared: null | string[] = null
+
+  try {
+    shared = sharedGatewayProfiles(await getStatus())
+  } catch {
+    // Status unavailable: fall back to the plain restart rather than blocking it.
+    return null
+  }
+
+  if (!shared) {
+    return null
+  }
+
+  const ok = await confirm({
+    title: translateNow('commandCenter.sharedGatewayRestartTitle'),
+    description: translateNow('commandCenter.sharedGatewayRestartDescription', shared.join(', ')),
+    confirmLabel: translateNow('commandCenter.sharedGatewayRestartConfirm'),
+    cancelLabel: translateNow('common.cancel'),
+    destructive: true
+  })
+
+  return ok ? shared : false
+}
+
 // Restart the messaging gateway, surfacing progress in the statusbar gateway
 // indicator. Self-contained and never rejects, so every trigger — Cmd+K, the
 // messaging save/toggle toasts — gets identical feedback from a plain
@@ -41,11 +74,21 @@ async function awaitAction(name: string): Promise<void> {
 // Resolves `true` when the restart child completed cleanly (callers that keep
 // a "restart needed" banner clear it on that signal only).
 export async function runGatewayRestart(): Promise<boolean> {
+  const shared = await confirmSharedGatewayRestart()
+
+  if (shared === false) {
+    return false
+  }
+
   $gatewayRestarting.set(true)
 
   try {
     const started: ActionResponse = await restartGateway()
     await awaitAction(started.name)
+
+    if (shared) {
+      notify({ kind: 'success', message: translateNow('commandCenter.sharedGatewayRestarted', shared.length) })
+    }
 
     return true
   } catch (err) {

web/src/App.tsx
@@ -105,6 +105,7 @@ import type { PluginManifest } from "@/plugins";
 import { useTheme } from "@/themes";
 import { isDashboardEmbeddedChatEnabled } from "@/lib/dashboard-flags";
 import { latchChatActivation } from "@/lib/chat-activation";
+import { sharedGatewayProfiles, sharedGatewayRestartDescription } from "@/lib/shared-gateway";
 import { api } from "@/lib/api";
 import type { StatusResponse, UpdateCheckResponse } from "@/lib/api";
 
@@ -943,6 +944,8 @@ function SidebarSystemActions({
   const { activeAction, isBusy, isRunning, pendingAction, runAction } =
     useSystemActions();
   const canUpdateHermes = status?.can_update_hermes === true;
+  // Served by the shared multiplexer: a restart blips every bot on this device — say which.
+  const sharedGateway = sharedGatewayProfiles(status);
   const [restartConfirmOpen, setRestartConfirmOpen] = useState(false);
   const [updateConfirmOpen, setUpdateConfirmOpen] = useState(false);
   const [updateConfirmInfo, setUpdateConfirmInfo] =
@@ -1076,17 +1079,21 @@ function SidebarSystemActions({
 
     <ConfirmDialog
       cancelLabel={t.common.cancel}
-      confirmLabel={t.status.restartGateway}
+      confirmLabel={sharedGateway ? "Restart all" : t.status.restartGateway}
       description={
-        t.status.restartGatewayConfirmMessage ??
-        "This restarts the Hermes gateway process. Connected channels and active sessions will reconnect afterward."
+        sharedGateway
+          ? sharedGatewayRestartDescription(sharedGateway)
+          : (t.status.restartGatewayConfirmMessage ??
+            "This restarts the Hermes gateway process. Connected channels and active sessions will reconnect afterward.")
       }
       loading={pendingAction === "restart"}
       onCancel={() => setRestartConfirmOpen(false)}
       onConfirm={confirmRestart}
       open={restartConfirmOpen}
       title={
-        t.status.restartGatewayConfirmTitle ?? `${t.status.restartGateway}?`
+        sharedGateway
+          ? "Restart the shared gateway?"
+          : (t.status.restartGatewayConfirmTitle ?? `${t.status.restartGateway}?`)
       }
     />
 

loteiron/ZeusAgent

Upstream-Commit: 6a66a5d481c1976aadcf5423ce6af0a1d5a4727c
Upstream-Repository: https://github.com/NousResearch/hermes-agent

apps/desktop/src/lib/shared-gateway-restart.ts
@@ -1,0 +1,21 @@
+import type { StatusResponse } from '@/types/zeus'
+
+/** Profiles a gateway restart would blip when the polled profile is carried by the shared
+ *  multiplexer, in display order (default first, then as recorded). `null` for a standalone
+ *  gateway or an older backend that does not report `gateway_shared_with` — those keep the
+ *  plain no-dialog restart. A record naming only one profile is not shared either. */
+export function sharedGatewayProfiles(status: Pick<StatusResponse, 'gateway_shared_with'> | null | undefined): null | string[] {
+  const shared = status?.gateway_shared_with
+
+  if (!Array.isArray(shared)) {
+    return null
+  }
+
+  const names = [...new Set(shared.map(name => String(name).trim()).filter(Boolean))]
+
+  if (names.length < 2) {
+    return null
+  }
+
+  return names.sort((left, right) => (left === 'default' ? -1 : right === 'default' ? 1 : left.localeCompare(right)))
+}

apps/desktop/src/store/system-actions.ts
@@ -1,8 +1,10 @@
 import { atom } from 'nanostores'
 
-import { getActionStatus, restartGateway } from '@/zeus'
+import { getActionStatus, getStatus, restartGateway } from '@/zeus'
 import { translateNow } from '@/i18n'
-import { notifyError } from '@/store/notifications'
+import { sharedGatewayProfiles } from '@/lib/shared-gateway-restart'
+import { confirm } from '@/store/confirm'
+import { notify, notifyError } from '@/store/notifications'
 import type { ActionResponse } from '@/types/zeus'
 
 const POLL_ATTEMPTS = 18
@@ -31,17 +33,65 @@ async function awaitAction(started: ActionResponse): Promise<void> {
   }
 }
 
+// Under `gateway.multiplex_profiles` the profile in view has no gateway of its
+// own: "Restart gateway" restarts the ONE shared multiplexer and every bot on
+// this device blips. Ask first, naming them; standalone gateways (and older
+// backends that do not report `gateway_shared_with`) keep the silent restart.
+// Resolves the served list when the user confirmed, `null` when nothing is
+// shared, `false` when they cancelled.
+export async function confirmSharedGatewayRestart(): Promise<false | null | string[]> {
+  let shared: null | string[] = null
+
+  try {
+    shared = sharedGatewayProfiles(await getStatus())
+  } catch {
+    // Status unavailable: fall back to the plain restart rather than blocking it.
+    return null
+  }
+
+  if (!shared) {
+    return null
+  }
+
+  const ok = await confirm({
+    title: translateNow('commandCenter.sharedGatewayRestartTitle'),
+    description: translateNow('commandCenter.sharedGatewayRestartDescription', shared.join(', ')),
+    confirmLabel: translateNow('commandCenter.sharedGatewayRestartConfirm'),
+    cancelLabel: translateNow('common.cancel'),
+    destructive: true
+  })
+
+  return ok ? shared : false
+}
+
 // Restart the messaging gateway, surfacing progress in the statusbar gateway
 // indicator. Self-contained and never rejects, so every trigger — Cmd+K, the
 // messaging save/toggle toasts — gets identical feedback from a plain
 // `void runGatewayRestart()`, and a failure is the only thing that toasts.
-export async function runGatewayRestart(): Promise<void> {
+// Resolves `true` when the restart child completed cleanly (callers that keep
+// a "restart needed" banner clear it on that signal only).
+export async function runGatewayRestart(): Promise<boolean> {
+  const shared = await confirmSharedGatewayRestart()
+
+  if (shared === false) {
+    return false
+  }
+
   $gatewayRestarting.set(true)
 
   try {
-    await awaitAction(await restartGateway())
+    const started: ActionResponse = await restartGateway()
+    await awaitAction(started)
+
+    if (shared) {
+      notify({ kind: 'success', message: translateNow('commandCenter.sharedGatewayRestarted', shared.length) })
+    }
+
+    return true
   } catch (err) {
     notifyError(err, translateNow('commandCenter.gatewayRestartFailed'))
+
+    return false
   } finally {
     $gatewayRestarting.set(false)
   }

web/src/App.tsx
@@ -103,6 +103,7 @@ import type { PluginManifest } from "@/plugins";
 import { useTheme } from "@/themes";
 import { isDashboardEmbeddedChatEnabled } from "@/lib/dashboard-flags";
 import { latchChatActivation } from "@/lib/chat-activation";
+import { sharedGatewayProfiles, sharedGatewayRestartDescription } from "@/lib/shared-gateway";
 import { api } from "@/lib/api";
 import type { StatusResponse, UpdateCheckResponse } from "@/lib/api";
 
@@ -938,6 +939,8 @@ function SidebarSystemActions({
   const { activeAction, isBusy, isRunning, pendingAction, runAction } =
     useSystemActions();
   const canUpdateZeusAgent = status?.can_update_zeus === true;
+  // Served by the shared multiplexer: a restart blips every bot on this device — say which.
+  const sharedGateway = sharedGatewayProfiles(status);
   const [restartConfirmOpen, setRestartConfirmOpen] = useState(false);
   const [updateConfirmOpen, setUpdateConfirmOpen] = useState(false);
   const [updateConfirmInfo, setUpdateConfirmInfo] =
@@ -1071,17 +1074,21 @@ function SidebarSystemActions({
 
     <ConfirmDialog
       cancelLabel={t.common.cancel}
-      confirmLabel={t.status.restartGateway}
+      confirmLabel={sharedGateway ? "Restart all" : t.status.restartGateway}
       description={
-        t.status.restartGatewayConfirmMessage ??
-        "This restarts the ZeusAgent gateway process. Connected channels and active sessions will reconnect afterward."
+        sharedGateway
+          ? sharedGatewayRestartDescription(sharedGateway)
+          : (t.status.restartGatewayConfirmMessage ??
+            "This restarts the ZeusAgent gateway process. Connected channels and active sessions will reconnect afterward.")
       }
       loading={pendingAction === "restart"}
       onCancel={() => setRestartConfirmOpen(false)}
       onConfirm={confirmRestart}
       open={restartConfirmOpen}
       title={
-        t.status.restartGatewayConfirmTitle ?? `${t.status.restartGateway}?`
+        sharedGateway
+          ? "Restart the shared gateway?"
+          : (t.status.restartGatewayConfirmTitle ?? `${t.status.restartGateway}?`)
       }
     />
 