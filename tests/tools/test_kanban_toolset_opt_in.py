NousResearch/hermes-agent

fix(kanban): honor explicit platform tool opt-ins across configuration surfaces

tests/tools/test_kanban_toolset_opt_in.py
@@ -1,0 +1,138 @@
+"""Saved tool opt-ins must reach the real schema without leaking across platforms."""
+from __future__ import annotations
+
+import json
+from concurrent.futures import ThreadPoolExecutor
+from pathlib import Path
+
+import pytest
+
+
+def _names(selection, disabled=None):
+    from model_tools import get_tool_definitions
+
+    return {
+        row["function"]["name"]
+        for row in get_tool_definitions(
+            selection, disabled_toolsets=disabled, quiet_mode=True,
+            skip_tool_search_assembly=True,
+        )
+        if row["function"]["name"].startswith("kanban_")
+    }
+
+
+@pytest.mark.parametrize("surface", ["cli", "http", "rpc"])
+def test_saved_opt_in_roundtrip_reaches_schema_and_board(surface, tmp_path, monkeypatch):
+    """Exercise the real config writer, availability gate, skills gate and handler."""
+    monkeypatch.setattr(Path, "home", lambda: tmp_path)
+    monkeypatch.delenv("HERMES_KANBAN_TASK", raising=False)
+    monkeypatch.delenv("HERMES_KANBAN_BOARD", raising=False)
+    from hermes_cli.config import load_config, save_config
+    from hermes_cli.tools_config import _apply_toolset_change, _get_platform_tools
+    from tools.registry import registry
+
+    save_config({"platform_toolsets": {"cli": ["file"], "telegram": ["file"]}})
+
+    def selected(platform="cli"):
+        return sorted(_get_platform_tools(load_config(), platform, include_default_mcp_servers=False))
+
+    before = _names(selected())  # Warm both cache layers before enabling.
+    assert not before
+    client = None
+    if surface == "http":
+        from fastapi import FastAPI
+        from fastapi.testclient import TestClient
+        from hermes_cli.web_routers.tools import router
+
+        app = FastAPI()
+        app.include_router(router)
+        client = TestClient(app)
+
+    def toggle(enabled):
+        action = "enable" if enabled else "disable"
+        if surface == "http":
+            response = client.put("/api/tools/toolsets/kanban", json={"enabled": enabled})
+            assert response.status_code == 200, response.text
+            assert response.json()["enabled"] is enabled
+        elif surface == "rpc":
+            from tui_gateway.server import _methods
+
+            response = _methods["tools.configure"]("kanban-opt-in", {"action": action, "names": ["kanban"]})
+            assert "error" not in response, response
+            assert "kanban" in response["result"]["changed"]
+        else:
+            _apply_toolset_change(load_config(), "cli", ["kanban"], action)
+
+    try:
+        toggle(True)
+        enabled_names = _names(selected())
+        assert {"kanban_list", "kanban_create", "kanban_complete"} <= enabled_names
+        assert not _names(selected("telegram")), "CLI opt-in leaked to Telegram"
+        assert "file" in selected()
+        # A second profile in the same process must not borrow this grant or
+        # poison the first profile's cached schema on return.
+        from hermes_constants import set_hermes_home_override, reset_hermes_home_override
+        other_home = tmp_path / "profiles" / "observer"
+        token = set_hermes_home_override(other_home)
+        try:
+            save_config({"platform_toolsets": {"cli": ["file"]}})
+            assert not _names(selected())
+        finally:
+            reset_hermes_home_override(token)
+        assert _names(selected()) == enabled_names
+        from agent.skill_utils import _detect_kanban
+        assert _detect_kanban(), "Saved opt-in still hides the Kanban playbook"
+        result = json.loads(registry.dispatch("kanban_create", {"title": "opt-in roundtrip", "assignee": "default"}))
+        assert result.get("ok"), result
+        from hermes_cli.kanban_db_connect import connect_closing
+        from hermes_cli.kanban_db import get_task
+        with connect_closing() as conn:
+            assert get_task(conn, result["task_id"]).title == "opt-in roundtrip"
+        toggle(False)
+        assert not _names(selected())
+        assert not _detect_kanban()
+        assert "file" in selected()
+        assert enabled_names, "Changing config must not mutate an already-built schema"
+    finally:
+        if client is not None:
+            client.close()
+
+
+@pytest.mark.parametrize("legacy", [False, True])
+def test_selection_is_scoped_and_preserves_worker_and_deny_boundaries(legacy, tmp_path, monkeypatch):
+    monkeypatch.setattr(Path, "home", lambda: tmp_path)
+    monkeypatch.delenv("HERMES_KANBAN_TASK", raising=False)
+    monkeypatch.delenv("HERMES_KANBAN_BOARD", raising=False)
+    from hermes_cli.config import load_config, save_config
+    from hermes_cli.tools_config import _get_platform_tools
+    from agent.delegation_context import delegated_child_context
+
+    save_config({"toolsets": ["kanban"] if legacy else [], "platform_toolsets": {"telegram": ["kanban"]}})
+    # The same profile concurrently builds an explicitly opted-in schema and
+    # an all/default schema. A platform grant must not become a cached global grant.
+    with ThreadPoolExecutor(max_workers=2) as pool:
+        named, broad = list(pool.map(_names, [["kanban"], ["hermes-cli"]]))
+    assert "kanban_create" in named
+    assert bool(broad) is legacy
+    assert bool(_names(None)) is legacy
+    assert bool(_names(["all"])) is legacy
+    assert not _names(["kanban"], ["kanban"])
+    assert not _names([])
+    assert bool(_names(sorted(_get_platform_tools(load_config(), "cli")))) is legacy
+
+    # Explicitly empty saved configuration wins over a legacy fallback, including
+    # the actual Desktop/TUI loader (which must not turn [] back into None/all).
+    cfg = load_config()
+    cfg["platform_toolsets"]["cli"] = []
+    save_config(cfg)
+    monkeypatch.delenv("HERMES_TUI_TOOLSETS", raising=False)
+    from tui_gateway.server import _load_enabled_toolsets
+    assert not _names(_load_enabled_toolsets("desktop"))
+
+    monkeypatch.setenv("HERMES_KANBAN_TASK", "t_worker")
+    worker = _names(["file"])
+    assert "kanban_complete" in worker
+    assert "kanban_list" not in worker
+    with delegated_child_context():
+        assert not _names(["kanban"])
+    assert "kanban_complete" in _names(["file"])

tests/hermes_cli/test_kanban_worker_spawn_toolsets.py
@@ -162,5 +162,12 @@ toolsets:
     assert resolved is not None
     assert "terminal" in resolved
     assert "web" in resolved
-    assert "kanban" in resolved  # recovered worker lifecycle surface
+    # Opt-in is no longer inferred for ordinary chats. The dispatcher-owned
+    # worker gets lifecycle tools at schema assembly, independently of the
+    # assignee's saved chat selection.
+    monkeypatch.setenv("HERMES_KANBAN_TASK", "t_spawn_tools")
+    from model_tools import get_tool_definitions
+    names = {t["function"]["name"] for t in get_tool_definitions(resolved, quiet_mode=True, skip_tool_search_assembly=True)}
+    assert "kanban_complete" in names
+    assert "kanban_list" not in names
     assert resolved != ["kanban"]

tests/hermes_cli/test_tools_config.py
@@ -848,38 +848,18 @@ def test_get_effective_configurable_toolsets_dedupes_bundled_plugins():
 
 
 
-# ── Checklist diff scope: non-configurable toolsets (kanban) must not be
-#    reported as added/removed by `hermes tools` ──────────────────────────
-
-
-
-
-def test_kanban_not_reported_as_removed_in_diff():
-    """Reproduces the false-signal bug: `hermes tools` printed ``- kanban``
-    when saving a platform that resolves kanban as enabled, even though the
-    checklist never offered kanban as a toggle.
-
-    The printed diff must be scoped to ``_checklist_toolset_keys`` so a tool
-    the user could not deselect is never reported as removed. The persisted
-    config still keeps kanban (verified separately by _save_platform_tools).
-    """
+# Kanban now participates in the checklist: an explicit deselection must be
+# both visible in the diff and durable in the platform selection.
+def test_kanban_checklist_reports_and_persists_explicit_removal():
     config = {"platform_toolsets": {"telegram": ["kanban", "web", "terminal"]}}
     current = _get_platform_tools(config, "telegram", include_default_mcp_servers=False)
-    assert "kanban" in current  # resolved as enabled at read time
-
-    # The checklist can only return configurable keys it was shown; kanban
-    # is never one of them.
     universe = _checklist_toolset_keys("telegram")
-    new_enabled = {t for t in current if t != "kanban"}
-
-    # Unscoped (old, buggy) diff would surface kanban.
-    assert (current - new_enabled) == {"kanban"}
-    # Scoped (fixed) diff drops it.
-    assert ((current - new_enabled) & universe) == set()
-
-
-
-
+    new_enabled = current - {"kanban"}
+    assert ((current - new_enabled) & universe) == {"kanban"}
+    with patch("hermes_cli.tools_config.save_config"):
+        _save_platform_tools(config, "telegram", new_enabled)
+    assert "kanban" not in _get_platform_tools(config, "telegram", include_default_mcp_servers=False)
+    assert {"web", "terminal"} <= set(config["platform_toolsets"]["telegram"])
 
 
 def test_vision_picker_custom_endpoint(tmp_path, monkeypatch):

tools/kanban_tools.py
@@ -17,7 +17,7 @@ from typing import Any, Callable, Optional
 
 from agent.redact import redact_sensitive_text
 from hermes_cli.goals import judge_goal
-from tools.registry import registry, tool_error
+from tools.registry import no_cache_check_fn, registry, tool_error
 from hermes_cli.config import cfg_get, load_config
 from tools.kanban_tools_schemas import (
     KANBAN_ATTACH_SCHEMA,
@@ -35,9 +35,28 @@ KANBAN_LIST_MAX_LIMIT = 200
 # --- Gating ---
 
 def _profile_has_kanban_toolset() -> bool:
-    # load_config() is mtime-cached and check_fn results are TTL-cached (~30s).
+    from tools.kanban_toolset_context import kanban_toolset_requested
+
+    requested = kanban_toolset_requested()
+    if requested:
+        return True
     try:
-        return "kanban" in load_config().get("toolsets", [])
+        config = load_config()
+        # Preserve the legacy profile-wide opt-in for callers using bundles.
+        if "kanban" in (config.get("toolsets") or []):
+            return True
+        if requested is not None:
+            # Never borrow another platform's opt-in during schema assembly.
+            return False
+        # Offer-time skill discovery has no platform selection. A saved opt-in
+        # makes the playbook relevant; actual schemas still use the scope above.
+        from hermes_cli.tools_config import _get_platform_tools
+
+        platforms = config.get("platform_toolsets") or {}
+        return any(
+            "kanban" in _get_platform_tools(config, platform, include_default_mcp_servers=False)
+            for platform, names in platforms.items() if isinstance(names, list)
+        )
     except Exception:
         return False
 
@@ -71,11 +90,13 @@ def _visible(*, to_env_worker: bool) -> bool:
     return _profile_has_kanban_toolset()
 
 
+@no_cache_check_fn
 def _check_kanban_mode() -> bool:
     """Lifecycle tools: dispatcher workers + profiles with the ``kanban`` toolset."""
     return _visible(to_env_worker=True)
 
 
+@no_cache_check_fn
 def _check_kanban_orchestrator_mode() -> bool:
     """Board-routing tools (kanban_list, kanban_unblock): hidden from task workers."""
     return _visible(to_env_worker=False)

loteiron/ZeusAgent

Upstream-Commit: 3d7f773bb42498d66ea4c0be1fe7d2e0d00278d7
Upstream-Repository: https://github.com/NousResearch/hermes-agent

tests/tools/test_kanban_toolset_opt_in.py
@@ -1,0 +1,138 @@
+"""Saved tool opt-ins must reach the real schema without leaking across platforms."""
+from __future__ import annotations
+
+import json
+from concurrent.futures import ThreadPoolExecutor
+from pathlib import Path
+
+import pytest
+
+
+def _names(selection, disabled=None):
+    from model_tools import get_tool_definitions
+
+    return {
+        row["function"]["name"]
+        for row in get_tool_definitions(
+            selection, disabled_toolsets=disabled, quiet_mode=True,
+            skip_tool_search_assembly=True,
+        )
+        if row["function"]["name"].startswith("kanban_")
+    }
+
+
+@pytest.mark.parametrize("surface", ["cli", "http", "rpc"])
+def test_saved_opt_in_roundtrip_reaches_schema_and_board(surface, tmp_path, monkeypatch):
+    """Exercise the real config writer, availability gate, skills gate and handler."""
+    monkeypatch.setattr(Path, "home", lambda: tmp_path)
+    monkeypatch.delenv("ZEUS_KANBAN_TASK", raising=False)
+    monkeypatch.delenv("ZEUS_KANBAN_BOARD", raising=False)
+    from zeus_cli.config import load_config, save_config
+    from zeus_cli.tools_config import _apply_toolset_change, _get_platform_tools
+    from tools.registry import registry
+
+    save_config({"platform_toolsets": {"cli": ["file"], "telegram": ["file"]}})
+
+    def selected(platform="cli"):
+        return sorted(_get_platform_tools(load_config(), platform, include_default_mcp_servers=False))
+
+    before = _names(selected())  # Warm both cache layers before enabling.
+    assert not before
+    client = None
+    if surface == "http":
+        from fastapi import FastAPI
+        from fastapi.testclient import TestClient
+        from zeus_cli.web_routers.tools import router
+
+        app = FastAPI()
+        app.include_router(router)
+        client = TestClient(app)
+
+    def toggle(enabled):
+        action = "enable" if enabled else "disable"
+        if surface == "http":
+            response = client.put("/api/tools/toolsets/kanban", json={"enabled": enabled})
+            assert response.status_code == 200, response.text
+            assert response.json()["enabled"] is enabled
+        elif surface == "rpc":
+            from tui_gateway.server import _methods
+
+            response = _methods["tools.configure"]("kanban-opt-in", {"action": action, "names": ["kanban"]})
+            assert "error" not in response, response
+            assert "kanban" in response["result"]["changed"]
+        else:
+            _apply_toolset_change(load_config(), "cli", ["kanban"], action)
+
+    try:
+        toggle(True)
+        enabled_names = _names(selected())
+        assert {"kanban_list", "kanban_create", "kanban_complete"} <= enabled_names
+        assert not _names(selected("telegram")), "CLI opt-in leaked to Telegram"
+        assert "file" in selected()
+        # A second profile in the same process must not borrow this grant or
+        # poison the first profile's cached schema on return.
+        from zeus_constants import set_zeus_home_override, reset_zeus_home_override
+        other_home = tmp_path / "profiles" / "observer"
+        token = set_zeus_home_override(other_home)
+        try:
+            save_config({"platform_toolsets": {"cli": ["file"]}})
+            assert not _names(selected())
+        finally:
+            reset_zeus_home_override(token)
+        assert _names(selected()) == enabled_names
+        from agent.skill_utils import _detect_kanban
+        assert _detect_kanban(), "Saved opt-in still hides the Kanban playbook"
+        result = json.loads(registry.dispatch("kanban_create", {"title": "opt-in roundtrip", "assignee": "default"}))
+        assert result.get("ok"), result
+        from zeus_cli.kanban_db_connect import connect_closing
+        from zeus_cli.kanban_db import get_task
+        with connect_closing() as conn:
+            assert get_task(conn, result["task_id"]).title == "opt-in roundtrip"
+        toggle(False)
+        assert not _names(selected())
+        assert not _detect_kanban()
+        assert "file" in selected()
+        assert enabled_names, "Changing config must not mutate an already-built schema"
+    finally:
+        if client is not None:
+            client.close()
+
+
+@pytest.mark.parametrize("legacy", [False, True])
+def test_selection_is_scoped_and_preserves_worker_and_deny_boundaries(legacy, tmp_path, monkeypatch):
+    monkeypatch.setattr(Path, "home", lambda: tmp_path)
+    monkeypatch.delenv("ZEUS_KANBAN_TASK", raising=False)
+    monkeypatch.delenv("ZEUS_KANBAN_BOARD", raising=False)
+    from zeus_cli.config import load_config, save_config
+    from zeus_cli.tools_config import _get_platform_tools
+    from agent.delegation_context import delegated_child_context
+
+    save_config({"toolsets": ["kanban"] if legacy else [], "platform_toolsets": {"telegram": ["kanban"]}})
+    # The same profile concurrently builds an explicitly opted-in schema and
+    # an all/default schema. A platform grant must not become a cached global grant.
+    with ThreadPoolExecutor(max_workers=2) as pool:
+        named, broad = list(pool.map(_names, [["kanban"], ["zeus-cli"]]))
+    assert "kanban_create" in named
+    assert bool(broad) is legacy
+    assert bool(_names(None)) is legacy
+    assert bool(_names(["all"])) is legacy
+    assert not _names(["kanban"], ["kanban"])
+    assert not _names([])
+    assert bool(_names(sorted(_get_platform_tools(load_config(), "cli")))) is legacy
+
+    # Explicitly empty saved configuration wins over a legacy fallback, including
+    # the actual Desktop/TUI loader (which must not turn [] back into None/all).
+    cfg = load_config()
+    cfg["platform_toolsets"]["cli"] = []
+    save_config(cfg)
+    monkeypatch.delenv("ZEUS_TUI_TOOLSETS", raising=False)
+    from tui_gateway.server import _load_enabled_toolsets
+    assert not _names(_load_enabled_toolsets("desktop"))
+
+    monkeypatch.setenv("ZEUS_KANBAN_TASK", "t_worker")
+    worker = _names(["file"])
+    assert "kanban_complete" in worker
+    assert "kanban_list" not in worker
+    with delegated_child_context():
+        assert not _names(["kanban"])
+    assert "kanban_complete" in _names(["file"])

tests/zeus_cli/test_kanban_worker_spawn_toolsets.py
@@ -162,5 +162,12 @@ toolsets:
     assert resolved is not None
     assert "terminal" in resolved
     assert "web" in resolved
-    assert "kanban" in resolved  # recovered worker lifecycle surface
+    # Opt-in is no longer inferred for ordinary chats. The dispatcher-owned
+    # worker gets lifecycle tools at schema assembly, independently of the
+    # assignee's saved chat selection.
+    monkeypatch.setenv("ZEUS_KANBAN_TASK", "t_spawn_tools")
+    from model_tools import get_tool_definitions
+    names = {t["function"]["name"] for t in get_tool_definitions(resolved, quiet_mode=True, skip_tool_search_assembly=True)}
+    assert "kanban_complete" in names
+    assert "kanban_list" not in names
     assert resolved != ["kanban"]

tests/zeus_cli/test_tools_config.py
@@ -848,38 +848,18 @@ def test_get_effective_configurable_toolsets_dedupes_bundled_plugins():
 
 
 
-# ── Checklist diff scope: non-configurable toolsets (kanban) must not be
-#    reported as added/removed by `zeus tools` ──────────────────────────
-
-
-
-
-def test_kanban_not_reported_as_removed_in_diff():
-    """Reproduces the false-signal bug: `zeus tools` printed ``- kanban``
-    when saving a platform that resolves kanban as enabled, even though the
-    checklist never offered kanban as a toggle.
-
-    The printed diff must be scoped to ``_checklist_toolset_keys`` so a tool
-    the user could not deselect is never reported as removed. The persisted
-    config still keeps kanban (verified separately by _save_platform_tools).
-    """
+# Kanban now participates in the checklist: an explicit deselection must be
+# both visible in the diff and durable in the platform selection.
+def test_kanban_checklist_reports_and_persists_explicit_removal():
     config = {"platform_toolsets": {"telegram": ["kanban", "web", "terminal"]}}
     current = _get_platform_tools(config, "telegram", include_default_mcp_servers=False)
-    assert "kanban" in current  # resolved as enabled at read time
-
-    # The checklist can only return configurable keys it was shown; kanban
-    # is never one of them.
     universe = _checklist_toolset_keys("telegram")
-    new_enabled = {t for t in current if t != "kanban"}
-
-    # Unscoped (old, buggy) diff would surface kanban.
-    assert (current - new_enabled) == {"kanban"}
-    # Scoped (fixed) diff drops it.
-    assert ((current - new_enabled) & universe) == set()
-
-
-
-
+    new_enabled = current - {"kanban"}
+    assert ((current - new_enabled) & universe) == {"kanban"}
+    with patch("zeus_cli.tools_config.save_config"):
+        _save_platform_tools(config, "telegram", new_enabled)
+    assert "kanban" not in _get_platform_tools(config, "telegram", include_default_mcp_servers=False)
+    assert {"web", "terminal"} <= set(config["platform_toolsets"]["telegram"])
 
 
 def test_vision_picker_custom_endpoint(tmp_path, monkeypatch):

tools/kanban_tools.py
@@ -17,7 +17,7 @@ from typing import Any, Callable, Optional
 
 from agent.redact import redact_sensitive_text
 from zeus_cli.goals import judge_goal
-from tools.registry import registry, tool_error
+from tools.registry import no_cache_check_fn, registry, tool_error
 from zeus_cli.config import cfg_get, load_config
 from tools.kanban_tools_schemas import (
     KANBAN_ATTACH_SCHEMA,
@@ -35,9 +35,28 @@ KANBAN_LIST_MAX_LIMIT = 200
 # --- Gating ---
 
 def _profile_has_kanban_toolset() -> bool:
-    # load_config() is mtime-cached and check_fn results are TTL-cached (~30s).
+    from tools.kanban_toolset_context import kanban_toolset_requested
+
+    requested = kanban_toolset_requested()
+    if requested:
+        return True
     try:
-        return "kanban" in load_config().get("toolsets", [])
+        config = load_config()
+        # Preserve the legacy profile-wide opt-in for callers using bundles.
+        if "kanban" in (config.get("toolsets") or []):
+            return True
+        if requested is not None:
+            # Never borrow another platform's opt-in during schema assembly.
+            return False
+        # Offer-time skill discovery has no platform selection. A saved opt-in
+        # makes the playbook relevant; actual schemas still use the scope above.
+        from zeus_cli.tools_config import _get_platform_tools
+
+        platforms = config.get("platform_toolsets") or {}
+        return any(
+            "kanban" in _get_platform_tools(config, platform, include_default_mcp_servers=False)
+            for platform, names in platforms.items() if isinstance(names, list)
+        )
     except Exception:
         return False
 
@@ -71,11 +90,13 @@ def _visible(*, to_env_worker: bool) -> bool:
     return _profile_has_kanban_toolset()
 
 
+@no_cache_check_fn
 def _check_kanban_mode() -> bool:
     """Lifecycle tools: dispatcher workers + profiles with the ``kanban`` toolset."""
     return _visible(to_env_worker=True)
 
 
+@no_cache_check_fn
 def _check_kanban_orchestrator_mode() -> bool:
     """Board-routing tools (kanban_list, kanban_unblock): hidden from task workers."""
     return _visible(to_env_worker=False)