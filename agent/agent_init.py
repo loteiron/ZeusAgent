Finn763/hermes-agent

fix(context): re-derive model.context_length on model/provider change
model.context_length is the user's profile-wide ceiling. It was read from
config.yaml in exactly one place — agent construction — and cached twice:
agent._config_context_length (switch/fallback resolution plus every display and
/usage surface) and context_compressor._config_context_length (the compressor's
own re-resolution).

Every live path that re-resolved a runtime then touched only one copy, or cleared
it without re-reading the config:

- switch_model nulled agent._config_context_length and re-derived the intent from
  custom_providers metadata alone, so a ceiling that only exists as
  model.context_length was dropped for the rest of the process;
- the Desktop/TUI compression hot-reload updated the compressor's copy only, so an
  open session showed a pinned ceiling while compressing against provider
  metadata / the 256K fallback.

Both now route through one pair of helpers in agent/agent_init.py:
set_config_context_length (one place that knows where the pin is cached) and
config_context_length_for_runtime (re-read from live config, scoped exactly like
construction, so an unrelated route still never inherits the pin).

agent/agent_init.py
@@ -1686,6 +1686,55 @@ def _scope_context_length_to_default_runtime(
     return _config_context_length
 
 
+def set_config_context_length(agent, value: Optional[int]) -> None:
+    """Store the durable ``model.context_length`` pin on EVERY cached copy of it.
+
+    The pin is read from config exactly once, at construction, then cached twice: on
+    ``agent._config_context_length`` (switch/fallback resolution plus every display and ``/usage``
+    surface) and on ``context_compressor._config_context_length`` (the compressor's own
+    re-resolution). Live paths that updated only one copy left the other stale, so a session could
+    report a pinned ceiling while compressing against a different window (#116467).
+    """
+    agent._config_context_length = value
+    _compressor = getattr(agent, "context_compressor", None)
+    if _compressor is not None:
+        _compressor._config_context_length = value
+
+
+def config_context_length_for_runtime(agent, config=None) -> Optional[int]:
+    """Re-read the durable ``model.context_length`` pin for ``agent``'s CURRENT runtime, or ``None``.
+
+    Single re-derivation point for the cached pin: construction resolves it once, and every live path
+    that re-resolves a runtime used to clear the cached copy without re-reading the config — so a
+    model/provider switch or a Desktop config round-trip silently dropped a ceiling the user still had
+    on disk, and resolution fell through to probing / catalog metadata / the 256K fallback (#116467).
+
+    Reuses construction's own scoping (``_scope_context_length_to_default_runtime``): the pin describes
+    the configured default route, so an unrelated runtime never inherits it.
+    """
+    try:
+        from hermes_cli.config import get_compatible_custom_providers, load_config
+        _agent_cfg = config if isinstance(config, dict) else load_config()
+        if not isinstance(_agent_cfg, dict):
+            return None
+        _model_section = _agent_cfg.get("model", {})
+        if not isinstance(_model_section, dict):
+            return None
+        _pin = _model_section.get("context_length")
+        if _pin is None or isinstance(_pin, bool):
+            return None
+        _pin = int(_pin)
+        if _pin <= 0:
+            return None
+        return _scope_context_length_to_default_runtime(
+            agent, _agent_cfg, _model_section, get_compatible_custom_providers(_agent_cfg),
+            _pin, str(getattr(agent, "base_url", "") or ""),
+        )
+    except Exception:
+        logger.debug("Could not re-read model.context_length for the current runtime", exc_info=True)
+        return None
+
+
 _CTX_LEN_REQUIREMENT = "must be a positive integer (e.g. 256000, not '256K')"
 
 

tests/agent/test_switch_model_reapplies_config_context_pin.py
@@ -1,0 +1,105 @@
+"""A live model/provider switch must re-derive the durable ``model.context_length`` pin.
+
+``model.context_length`` is the user's profile-wide ceiling. It is read from config.yaml exactly once,
+at agent construction (``agent_init._resolve_context_length``), then cached on
+``agent._config_context_length``. Every live path that re-resolves a runtime cleared that cached copy
+instead of re-reading the config, so a long-running Desktop/CLI session silently lost the ceiling and
+fell through to provider probing / catalog metadata / the 256K fallback — while the ceiling was still
+on disk and still shown by other surfaces.
+"""
+
+from unittest.mock import MagicMock, patch
+
+from agent.context_compressor import ContextCompressor
+from run_agent import AIAgent
+
+PIN = 249_000
+ROUTE = "http://127.0.0.1:8123/v1"
+OTHER_ROUTE = "https://openrouter.ai/api/v1"
+
+
+def _cfg_with_pin():
+    return {
+        "model": {
+            "default": "model-b",
+            "provider": "custom:acme",
+            "base_url": ROUTE,
+            "context_length": PIN,
+        },
+        "custom_providers": [{"name": "acme", "base_url": ROUTE, "models": {}}],
+    }
+
+
+def _make_agent(config_context_length=None):
+    agent = AIAgent.__new__(AIAgent)
+    agent.model = "model-a"
+    agent.provider = "openrouter"
+    agent.base_url = OTHER_ROUTE
+    agent.api_key = "sk-primary"
+    agent.api_mode = "chat_completions"
+    agent.client = MagicMock()
+    agent.quiet_mode = True
+    agent._config_context_length = config_context_length
+    agent._custom_providers = []
+    agent._primary_runtime = {}
+    agent._create_openai_client = lambda *_args, **_kwargs: MagicMock()
+    agent.context_compressor = ContextCompressor(
+        model="model-a", threshold_percent=0.50, base_url=OTHER_ROUTE,
+        api_key="sk-primary", provider="openrouter", quiet_mode=True,
+    )
+    return agent
+
+
+def _switch(agent, model, provider, base_url):
+    """Switch and return the ``config_context_length`` the compressor resolution ran with."""
+    with patch("agent.model_metadata.get_model_context_length", return_value=200_000) as ctx_len:
+        agent.switch_model(model, provider, api_key="sk-new", base_url=base_url)
+    return ctx_len.call_args.kwargs.get("config_context_length")
+
+
+def test_switch_to_configured_default_route_reapplies_context_pin():
+    """Switching onto the route ``model.context_length`` describes keeps the ceiling."""
+    cfg = _cfg_with_pin()
+    agent = _make_agent(config_context_length=None)
+
+    with (
+        patch("hermes_cli.config.load_config", return_value=cfg),
+        patch("hermes_cli.config.load_config_readonly", return_value=cfg),
+    ):
+        applied = _switch(agent, "model-b", "custom:acme", ROUTE)
+
+    assert applied == PIN
+    assert agent._config_context_length == PIN
+
+
+def test_switch_back_to_configured_default_route_restores_context_pin():
+    """A round-trip away from and back onto the configured route restores the ceiling."""
+    cfg = _cfg_with_pin()
+    agent = _make_agent(config_context_length=PIN)
+
+    with (
+        patch("hermes_cli.config.load_config", return_value=cfg),
+        patch("hermes_cli.config.load_config_readonly", return_value=cfg),
+        patch("agent.model_metadata.get_model_context_length", return_value=1_000_000),
+    ):
+        agent.switch_model("other-model", "openrouter", api_key="sk-new", base_url=OTHER_ROUTE)
+        assert agent._config_context_length is None  # pin does not describe the other route
+        applied = _switch(agent, "model-b", "custom:acme", ROUTE)
+
+    assert applied == PIN
+    assert agent._config_context_length == PIN
+
+
+def test_switch_to_other_route_does_not_inherit_context_pin():
+    """The pin stays scoped to its own route (no leak onto an unrelated model)."""
+    cfg = _cfg_with_pin()
+    agent = _make_agent(config_context_length=PIN)
+
+    with (
+        patch("hermes_cli.config.load_config", return_value=cfg),
+        patch("hermes_cli.config.load_config_readonly", return_value=cfg),
+    ):
+        applied = _switch(agent, "unrelated-model", "openrouter", OTHER_ROUTE)
+
+    assert applied is None
+    assert agent._config_context_length is None

loteiron/ZeusAgent

(cherry picked from commit 986ff16dadb9966f7328e55f295af5cfa1eb5c88)
Adapted for ZeusAgent.
Upstream-Commit: 36b6efc29750b2a741eb29e9ca2e8ddbbaa23770
Upstream-Repository: https://github.com/NousResearch/hermes-agent

agent/agent_init.py
@@ -1648,6 +1648,55 @@ def _scope_context_length_to_default_runtime(
     return _config_context_length
 
 
+def set_config_context_length(agent, value: Optional[int]) -> None:
+    """Store the durable ``model.context_length`` pin on EVERY cached copy of it.
+
+    The pin is read from config exactly once, at construction, then cached twice: on
+    ``agent._config_context_length`` (switch/fallback resolution plus every display and ``/usage``
+    surface) and on ``context_compressor._config_context_length`` (the compressor's own
+    re-resolution). Live paths that updated only one copy left the other stale, so a session could
+    report a pinned ceiling while compressing against a different window (#116467).
+    """
+    agent._config_context_length = value
+    _compressor = getattr(agent, "context_compressor", None)
+    if _compressor is not None:
+        _compressor._config_context_length = value
+
+
+def config_context_length_for_runtime(agent, config=None) -> Optional[int]:
+    """Re-read the durable ``model.context_length`` pin for ``agent``'s CURRENT runtime, or ``None``.
+
+    Single re-derivation point for the cached pin: construction resolves it once, and every live path
+    that re-resolves a runtime used to clear the cached copy without re-reading the config — so a
+    model/provider switch or a Desktop config round-trip silently dropped a ceiling the user still had
+    on disk, and resolution fell through to probing / catalog metadata / the 256K fallback (#116467).
+
+    Reuses construction's own scoping (``_scope_context_length_to_default_runtime``): the pin describes
+    the configured default route, so an unrelated runtime never inherits it.
+    """
+    try:
+        from zeus_cli.config import get_compatible_custom_providers, load_config
+        _agent_cfg = config if isinstance(config, dict) else load_config()
+        if not isinstance(_agent_cfg, dict):
+            return None
+        _model_section = _agent_cfg.get("model", {})
+        if not isinstance(_model_section, dict):
+            return None
+        _pin = _model_section.get("context_length")
+        if _pin is None or isinstance(_pin, bool):
+            return None
+        _pin = int(_pin)
+        if _pin <= 0:
+            return None
+        return _scope_context_length_to_default_runtime(
+            agent, _agent_cfg, _model_section, get_compatible_custom_providers(_agent_cfg),
+            _pin, str(getattr(agent, "base_url", "") or ""),
+        )
+    except Exception:
+        logger.debug("Could not re-read model.context_length for the current runtime", exc_info=True)
+        return None
+
+
 _CTX_LEN_REQUIREMENT = "must be a positive integer (e.g. 256000, not '256K')"
 
 

tests/agent/test_switch_model_reapplies_config_context_pin.py
@@ -1,0 +1,105 @@
+"""A live model/provider switch must re-derive the durable ``model.context_length`` pin.
+
+``model.context_length`` is the user's profile-wide ceiling. It is read from config.yaml exactly once,
+at agent construction (``agent_init._resolve_context_length``), then cached on
+``agent._config_context_length``. Every live path that re-resolves a runtime cleared that cached copy
+instead of re-reading the config, so a long-running Desktop/CLI session silently lost the ceiling and
+fell through to provider probing / catalog metadata / the 256K fallback — while the ceiling was still
+on disk and still shown by other surfaces.
+"""
+
+from unittest.mock import MagicMock, patch
+
+from agent.context_compressor import ContextCompressor
+from run_agent import AIAgent
+
+PIN = 249_000
+ROUTE = "http://127.0.0.1:8123/v1"
+OTHER_ROUTE = "https://openrouter.ai/api/v1"
+
+
+def _cfg_with_pin():
+    return {
+        "model": {
+            "default": "model-b",
+            "provider": "custom:acme",
+            "base_url": ROUTE,
+            "context_length": PIN,
+        },
+        "custom_providers": [{"name": "acme", "base_url": ROUTE, "models": {}}],
+    }
+
+
+def _make_agent(config_context_length=None):
+    agent = AIAgent.__new__(AIAgent)
+    agent.model = "model-a"
+    agent.provider = "openrouter"
+    agent.base_url = OTHER_ROUTE
+    agent.api_key = "sk-primary"
+    agent.api_mode = "chat_completions"
+    agent.client = MagicMock()
+    agent.quiet_mode = True
+    agent._config_context_length = config_context_length
+    agent._custom_providers = []
+    agent._primary_runtime = {}
+    agent._create_openai_client = lambda *_args, **_kwargs: MagicMock()
+    agent.context_compressor = ContextCompressor(
+        model="model-a", threshold_percent=0.50, base_url=OTHER_ROUTE,
+        api_key="sk-primary", provider="openrouter", quiet_mode=True,
+    )
+    return agent
+
+
+def _switch(agent, model, provider, base_url):
+    """Switch and return the ``config_context_length`` the compressor resolution ran with."""
+    with patch("agent.model_metadata.get_model_context_length", return_value=200_000) as ctx_len:
+        agent.switch_model(model, provider, api_key="sk-new", base_url=base_url)
+    return ctx_len.call_args.kwargs.get("config_context_length")
+
+
+def test_switch_to_configured_default_route_reapplies_context_pin():
+    """Switching onto the route ``model.context_length`` describes keeps the ceiling."""
+    cfg = _cfg_with_pin()
+    agent = _make_agent(config_context_length=None)
+
+    with (
+        patch("zeus_cli.config.load_config", return_value=cfg),
+        patch("zeus_cli.config.load_config_readonly", return_value=cfg),
+    ):
+        applied = _switch(agent, "model-b", "custom:acme", ROUTE)
+
+    assert applied == PIN
+    assert agent._config_context_length == PIN
+
+
+def test_switch_back_to_configured_default_route_restores_context_pin():
+    """A round-trip away from and back onto the configured route restores the ceiling."""
+    cfg = _cfg_with_pin()
+    agent = _make_agent(config_context_length=PIN)
+
+    with (
+        patch("zeus_cli.config.load_config", return_value=cfg),
+        patch("zeus_cli.config.load_config_readonly", return_value=cfg),
+        patch("agent.model_metadata.get_model_context_length", return_value=1_000_000),
+    ):
+        agent.switch_model("other-model", "openrouter", api_key="sk-new", base_url=OTHER_ROUTE)
+        assert agent._config_context_length is None  # pin does not describe the other route
+        applied = _switch(agent, "model-b", "custom:acme", ROUTE)
+
+    assert applied == PIN
+    assert agent._config_context_length == PIN
+
+
+def test_switch_to_other_route_does_not_inherit_context_pin():
+    """The pin stays scoped to its own route (no leak onto an unrelated model)."""
+    cfg = _cfg_with_pin()
+    agent = _make_agent(config_context_length=PIN)
+
+    with (
+        patch("zeus_cli.config.load_config", return_value=cfg),
+        patch("zeus_cli.config.load_config_readonly", return_value=cfg),
+    ):
+        applied = _switch(agent, "unrelated-model", "openrouter", OTHER_ROUTE)
+
+    assert applied is None
+    assert agent._config_context_length is None